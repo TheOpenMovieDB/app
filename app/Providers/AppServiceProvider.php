2189d5dba942c07165995fa746b60f646365f0bb
umair68 <umair@ik.me>
2025-06-22 20:59:46 +0200

    feat(app): add consistent defaults for models, dates, and CLI
    - Enabled strict mode for Eloquent models to catch attribute issues.
    - Set CarbonImmutable as default for date handling.
    - Prevent destructive DB commands in CLI when in production.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,17 +1,23 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
+final class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
     public function register(): void
     {
-        //
+
     }
 
     /**
@@ -19,6 +25,36 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->configureModels();
+        $this->configureDates();
+        $this->configureCli();
+
+
     }
+
+    /**
+     * Configure defaults for models for the application.
+     */
+    private function configureModels(): void
+    {
+        Model::shouldBeStrict(true);
+    }
+
+    /**
+     * Configure the dates for the application Immutable by default.
+     */
+    private function configureDates(): void
+    {
+        Date::use(CarbonImmutable::class);
+    }
+
+    /**
+     * Configure the default rules for cli commands.
+     */
+    private function configureCli(): void
+    {
+        DB::prohibitDestructiveCommands($this->app->isProduction());
+    }
+
+
 }
